ebe980f2aecbfa6d5d16b20fa9101ff2f75f12d2
chennyhaohao <chennyhaohao@gmail.com>
2018-04-26 00:42:19 +0400

    get accounts & deploying receipt system

--- client/src/contractController.js
+++ client/src/contractController.js
@@ -27,10 +27,31 @@ class Controller {
 		web3 = _web3;
 		this.ReceiptSystem.setProvider(web3.currentProvider);
 		web3versionFix(this.ReceiptSystem);
-		web3.eth.getAccounts(console.log);
+		web3.eth.getAccounts()
+			.then( accounts => {
+				this.account = accounts[0];
+				console.log("Account: ", this.account);
+				return this.account;
+			})
+			.then( acc => {
+				this.deployReceiptSystem();
+			});
 	}
 
-	
+	async deployReceiptSystem() {
+		try {
+			//this.coinbase = await web3.eth.getCoinbase();
+			console.log("Deploying Receipt System...");
+			var instance = await this.ReceiptSystem.new(
+				{from: this.account, gas: 4000000});
+			console.log("Receipt system deployed at: ", instance.address);
+			this.ReceiptSystemAddress = instance.address;
+	        return instance.address;
+		} catch(e) {
+			throw e;
+		}
+	}
+
 }
 
 var controller = new Controller();
