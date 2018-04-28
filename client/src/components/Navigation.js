4fca974074c8bb6625b919cfa654eb8603fbe8c0
chennyhaohao <chennyhaohao@gmail.com>
2018-04-29 00:29:34 +0400

    Its working

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -15,19 +15,19 @@ class NavigationComponent extends React.Component {
     render() {
     	var activeKey = null;
     	//console.log(window.location);
-    	if (window.location.href.includes('/verify')) {
+    	/*if (window.location.href.includes('/verify')) {
     		activeKey = 0;
     	} else if (window.location.href.includes('/issue')) {
     		activeKey = 1;
-    	}
+    	}*/
     	console.log(activeKey);
         return (
-            <Nav bsStyle="pills" activeKey={activeKey}>
+            <Nav bsStyle="tabs" activeKey={activeKey}>
                 
                 <LinkContainer to="/verify">
                 	<NavItem eventKey = {0}>Verification Interface</NavItem>
                 </LinkContainer> {" "}
-                
+
                 <LinkContainer to="/issue">
                 	<NavItem eventKey = {1}>Issue Interface</NavItem>
                 </LinkContainer>
